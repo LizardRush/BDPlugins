1a78e45bb54e22ee5344980f52bcdff3fe0b8cae
Lizard Rush <151949502+LizardRush@users.noreply.github.com>
2024-07-10 13:53:03 +0200

    Create AutoFullscreen.plugin.js

---
+++ plugins/AutoFullscreen.plugin.js
@@ -0,0 +1,22 @@
+/**
+ * @name AutoFullscreen
+ * @version 1.0.0
+ * @description Automatically makes Discord fullscreen on startup.
+ * @author Lizard Rush
+ */
+
+module.exports = class AutoFullscreen {
+    getName() { return "AutoFullscreen"; }
+    getDescription() { return "Automatically makes Discord fullscreen on startup."; }
+    getVersion() { return "1.0.0"; }
+    getAuthor() { return "LRush"; }
+
+    start() {
+        // Wait for Discord to fully load
+        setTimeout(() => {
+            const electron = require('electron');
+            const remote = electron.remote;
+            remote.getCurrentWindow().maximize();
+        }, 5000);
+    }
+};
